f15e64d1208031b55fff1e8c30b237be1e0be903
Edmond Nanushaj <33865566+EdmondN@users.noreply.github.com>
2018-02-21 19:47:40 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,17 +1,16 @@
 package com.example.android.quizapp;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
 import android.widget.Toast;
+
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     boolean resultQ2;
     boolean resultQ3;
     boolean resultQ4;
+    boolean resultQ5;
+    boolean resultQ6;
+
 
     int score = 0;
 
@@ -32,8 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //Create a toast message, displayed when a user fills in an answer.
-    CharSequence textToast = " Thanks for your answer!";
+
 
     //display toast
     public void onRadioButtonClickedQ1(View view) {
@@ -43,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId()) {
             case (R.id.radio1):
                 if (checked)
-                    resultQ4 = false;
+                    resultQ1 = false;
                 break;
             case R.id.radio2:
                 if (checked)
@@ -71,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.radiob2:
                 if (checked)
-                    resultQ1 = false;
+                    resultQ4 = false;
                 break;
             case R.id.radiob3:
                 if (checked)
@@ -81,11 +82,14 @@ public class MainActivity extends AppCompatActivity {
                 if (checked)
                     resultQ4 = true;
                 break;
-        }
+
+
+            }
+
     }
 
     public void onClear(View v) {
-       /* Clears all selected radio buttons to default */
+       /* Clears all selected radio buttons, checkboxes, edittexts to default */
         radio1Group = findViewById(R.id.radioGroup1);
         radio1Group.clearCheck();
         radio2Group = findViewById(R.id.radioGroup2);
@@ -106,11 +110,16 @@ public class MainActivity extends AppCompatActivity {
         checkBox3_3.setChecked(false);
         CheckBox checkBox3_4 = findViewById(R.id.checkboxb4);
         checkBox3_4.setChecked(false);
-        EditText resultQ5 = findViewById(R.id.edittext1);
-        resultQ5.setText("");
-        EditText resultQ6 = findViewById(R.id.edittext2);
-        resultQ6.setText("");
-
+        EditText question5 = findViewById(R.id.edittext1);
+        question5.setText("");
+        EditText question6 = findViewById(R.id.edittext2);
+        question6.setText("");
+        resultQ1 = false;
+        resultQ2 = false;
+        resultQ3 = false;
+        resultQ4 = false;
+        resultQ5 = false;
+        resultQ6 = false;
         score = 0;
     }
 
@@ -124,8 +133,24 @@ public class MainActivity extends AppCompatActivity {
         boolean question2_3 = question2__3.isChecked();
         CheckBox question2__4 = (CheckBox) findViewById(R.id.checkboxa4);
         boolean question2_4 = question2__4.isChecked();
-        if ((question2_1) && (question2_2) && (!question2_3) && (!question2_4)) {
+
+        // If RadioGroup is empty in question 1
+        RadioGroup radio1Group = findViewById(R.id.radioGroup1);
+        if (radio1Group.getCheckedRadioButtonId() == -1) {
+            score = 0;
+            Toast.makeText(this, R.string.noanswerQ1, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // If CheckBox is empty in question 2
+        if ((!question2_2) && (!question2_3) && (!question2_1) && (!question2_4)) {
+            score = 0;
+            Toast.makeText(this, R.string.noanswerQ2, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if ((question2_2) && (question2_3) && (!question2_1) && (!question2_4)) {
             resultQ2 = true;
+
         }
         CheckBox question3__1 = (CheckBox) findViewById(R.id.checkboxb1);
         boolean question3_1 = question3__1.isChecked();
@@ -135,17 +160,75 @@ public class MainActivity extends AppCompatActivity {
         boolean question3_3 = question3__3.isChecked();
         CheckBox question3__4 = (CheckBox) findViewById(R.id.checkboxb4);
         boolean question3_4 = question3__4.isChecked();
-        if ((question3_1) && (question3_2) && (!question3_3) && (!question3_4)) {
+
+        // If CheckBox is empty in question 3
+        if ((!question3_2) && (!question3_3) && (!question3_1) && (!question3_4)) {
+            score = 0;
+            Toast.makeText(this, R.string.noanswerQ3, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if ((question3_1) && (question3_3) && (!question3_2) && (!question3_4)) {
             resultQ3 = true;
+        }
+
+        // If RadioGroup is empty in question 4
+        RadioGroup radio2Group = findViewById(R.id.radioGroup2);
+        if (radio2Group.getCheckedRadioButtonId() == -1) {
+            score = 0;
+            Toast.makeText(this, R.string.noanswerQ4, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //Five question - right answer is: dias or zeus
+        EditText question_5 = findViewById(R.id.edittext1);
+        String question5 = question_5.getText().toString();
+        // If EditText is empty in question 5
+        if (question5.isEmpty()) {
+            score = 0;
+            Toast.makeText(this, R.string.noanswerQ5, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (question5.equals(getString(R.string.dias)) || question5.equals(getString(R.string.Dias)) || question5.equals(getString(R.string.zeus)) || question5.equals(getString(R.string.Zeus))) {
+            resultQ5 = true;
+        }
+        //Six question - right answer is: nafplio - Nafplio
+        EditText question_6 = findViewById(R.id.edittext2);
+        String question6 = question_6.getText().toString();
+        // If EditText is empty in question 6
+        if (question6.isEmpty()) {
+            score = 0;
+            Toast.makeText(this, R.string.noanswerQ6, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (question6.equals(getString(R.string.Nafplio1)) || question6.equals(getString(R.string.nafplio1)) || question6.equals(getString(R.string.nauplio)) || question6.equals(getString(R.string.Nauplio))) {
+            resultQ6 = true;
         }
 
 
         //Calls method to calculate the score.
-        int finalScore = calculateScore(score, resultQ1, resultQ2, resultQ3, resultQ4);
-        Toast.makeText(this, "Score:  " + finalScore + textToast, Toast.LENGTH_SHORT).show();
+        int finalScore = calculateScore(score, resultQ1, resultQ2, resultQ3, resultQ4 , resultQ5 , resultQ6 );
+        //Create a toast message, displayed when a user fills in an answer.
+        CharSequence textToast = "\n" + getString(R.string.thanks);
+        // Message if Questions is Correct or Wrong
+        String message = "";
+        if (resultQ1) { message += "\n" + getString(R.string.Q1isCorrect);}
+        else {message += "\n" + getString(R.string.Q1isWrong);}
+        if (resultQ2) { message += "\n" +  getString(R.string.Q2isCorrect);}
+        else {message +=  "\n" + getString(R.string.Q2isWrong);}
+        if (resultQ3) { message += "\n" +  getString(R.string.Q3isCorrect);}
+        else {message +=  "\n" +   getString(R.string.Q3isWrong);}
+        if (resultQ4) { message += "\n" +  getString(R.string.Q4isCorrect);}
+        else {message +=  "\n" +  getString(R.string.Q4isWrong);}
+        if (resultQ5) { message += "\n" +  getString(R.string.Q5isCorrect);}
+        else {message += "\n" +   getString(R.string.Q5isWrong);}
+        if (resultQ6) { message += "\n" +  getString(R.string.Q6isCorrect);}
+        else {message += "\n" +  getString(R.string.Q6isWrong);}
+        Toast.makeText(this, getString(R.string.score) + " " + finalScore + "\n" + textToast + "\n" + message, Toast.LENGTH_SHORT).show();
+
     }
 
-    public int calculateScore(int score, boolean resultQ1, boolean resultQ2, boolean resultQ3, boolean resultQ4) {
+    public int calculateScore(int score, boolean resultQ1, boolean resultQ2, boolean resultQ3, boolean resultQ4 , boolean resultQ5 ,boolean resultQ6 ) {
         if (resultQ1) {
             score += 1;
         }
@@ -158,22 +241,16 @@ public class MainActivity extends AppCompatActivity {
         if (resultQ4) {
             score += 1;
         }
-        if (resultQ5.equals("dias") || resultQ5.equals("Dias") || resultQ5.equals("zeus") || resultQ5.equals("Zeus")) {
+        if (resultQ5) {
             score += 1;
         }
-        if (resultQ6.equals("Nafplio") || resultQ6.equals("nafplio") || resultQ6.equals("nauplio") || resultQ6.equals("Nauplio")) {
+        if (resultQ6) {
             score += 1;
         }
-
 
         return (score);
     }
 
-    //Five question - right answer is: dias or zeus
-    EditText resultQ5 = findViewById(R.id.edittext1);
-
-    //Six question - right answer is: nafplio - Nafplio
-    EditText resultQ6 = findViewById(R.id.edittext2);
 
 }
 
